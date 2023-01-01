6adca89906c57b3ee6f4acb172aefe9c2e3019d6
sidharthK2 <sidharthkapoor.100@gmail.com>
2023-01-01 16:16:17 +0000

    feat: add list item function

--- contracts/NftMarketplace.sol
+++ contracts/NftMarketplace.sol
@@ -3,8 +3,11 @@ pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
+//Errors
 error NftMarketplace__PriceMustBeAboveZero();
 error NftMarketplace__NotApprovedForMarketplace();
+error NftMarketplace__AlreadyListed(address nftAddress, uint256 tokenId);
+error NftMarketplace__NotOwner();
 
 contract NftMarketplace {
     struct Listing {
@@ -12,6 +15,7 @@ contract NftMarketplace {
         address seller;
     }
 
+    //Events
     event ItemListed(
         address indexed seller,
         address indexed nftAddress,
@@ -22,12 +26,50 @@ contract NftMarketplace {
     //NFT contract => NFT token ID => Listing
     mapping(address => mapping(uint256 => Listing)) private s_listings;
 
+    //Modifiers
+    modifier notListed(
+        address nftAddress,
+        uint256 tokenId,
+        address owner
+    ) {
+        Listing memory listing = s_listings[nftAddress][tokenId];
+        if (listing.price > 0) {
+            revert NftMarketplace__AlreadyListed(nftAddress, tokenId);
+        }
+        _;
+    }
+
+    modifier isOwner(
+        address nftAddress,
+        uint256 tokenId,
+        address spender
+    ) {
+        IERC721 nft = IERC721(nftAddress);
+        address owner = nft.ownerOf(tokenId);
+        if (spender != owner) {
+            revert NftMarketplace__NotOwner();
+        }
+        _;
+    }
+
     //Main Functions
+
+    /*
+     * @notice Method for listing your NFT on the marketplace
+     * @param nftAddress: Address of the NFT
+     * @param tokenId: The Token ID of the NFT
+     * @param price: sale price of the listed NFT
+     * @dev Contract could be escrow but this way users can still hold their listed NFTs
+     */
     function listItem(
         address nftAddress,
         uint256 tokenId,
         uint256 price
-    ) external {
+    )
+        external
+        notListed(nftAddress, tokenId, msg.sender)
+        isOwner(nftAddress, tokenId, msg.sender)
+    {
         if (price <= 0) {
             revert NftMarketplace__PriceMustBeAboveZero();
         }
